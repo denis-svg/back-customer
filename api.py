a59b9ecfb9bf63e33e8bcc49579739a4e3124d15
denis <dprodan289@gmail.com>
2022-11-24 16:10:27 +0200

    locale endpoint

--- api.py
+++ api.py
@@ -4,7 +4,7 @@ import pyodbc
 app = Flask(__name__)
 
 @app.route('/api/statistics/clicksToConvert/Device', methods=['GET'])
-def ctc():
+def ctcD():
 	if request.method == 'GET':
 		cnxn = pyodbc.connect("""Driver={ODBC Driver 13 for SQL Server};
                         Server=tcp:prenaissance.database.windows.net,1433;
@@ -28,10 +28,45 @@ def ctc():
 										GROUP BY CAST(DATEPART(hour,clicked_date) AS INT)""").fetchall()
 			values = []
 			for entry in res:
-				values.append(entry[0])
+				values.append([entry[1], entry[0]])
 			out.append({"field":device, "values":values})
 		cnxn.close()
 		return jsonify(out)
+
+@app.route('/api/statistics/clicksToConvert/Locale', methods=['GET'])
+def ctcL():
+	if request.method == 'GET':
+		cnxn = pyodbc.connect("""Driver={ODBC Driver 13 for SQL Server};
+                        Server=tcp:prenaissance.database.windows.net,1433;
+                        Database=customerpp;Uid=alex;Pwd=Test1234;Encrypt=yes;
+                        TrustServerCertificate=no;Connection Timeout=30;""")
+		cursor = cnxn.cursor()
+
+		# selecting all devices
+		locales = cursor.execute(f"""SELECT 	TOP(15)
+												locale
+										FROM Persons
+										GROUP BY locale
+										ORDER BY COUNT(locale) DESC""").fetchall()
+
+		out = []
+		for locale in locales:
+			locale = locale[0]
+			res = cursor.execute(f"""SELECT COUNT(*),
+											CAST(DATEPART(hour,clicked_date) AS INT)
+										FROM Events
+										INNER JOIN Persons
+										ON Persons.person_id = events.person_id
+										WHERE Events.event_name = 'conversion' AND Persons.locale = '{locale}'
+										GROUP BY CAST(DATEPART(hour,clicked_date) AS INT)""").fetchall()
+			values = []
+			for entry in res:
+				values.append([entry[1], entry[0]])
+			out.append({"field":locale, "values":values})
+		cnxn.close()
+		return jsonify(out)
+
+
 
 if __name__ == '__main__':
 	app.run()
